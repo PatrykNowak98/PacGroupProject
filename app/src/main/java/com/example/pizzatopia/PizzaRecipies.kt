83f889079e78e8551c0b4cd2d4df77a1d8ad7f6f
Piasta <piastapiotr010@gmail.com>
2023-04-19 22:04:09 +0200

    sqlite

--- app/src/main/java/com/example/pizzatopia/PizzaRecipies.kt
+++ app/src/main/java/com/example/pizzatopia/PizzaRecipies.kt
@@ -3,31 +3,22 @@ package com.example.pizzatopia
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.sql.Connection
-import java.sql.ResultSet
-import java.sql.Statement
-import java.util.jar.Attributes.Name
 
 class PizzaRecipies : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pizza_recipies)
 
-
-
+        // Pobierz referencję do textView z widoku
         val textView = findViewById<TextView>(R.id.textView)
-        val c = ConSQL()
-        val connection: Connection = c.conclass()
-        if (c != null)
-        {
-            val sqlstatement="SELECT * FROM RECIPE"
-            val smt = connection.createStatement()
-            val set=smt.executeQuery(sqlstatement);
-            while (set.next())
-            {
-                textView.setText(set.getString(2));
-            }
-            connection.close();
-        }
+
+        // Tworzymy instancję SQLiteHelper
+        val dbHelper = SQLiteHelper(this)
+        // Wywołujemy funkcję getRecipe() z podanymi smakiem i ostrością
+        val SELECTFLAVOUR = "pepperoni"
+        val SELECTSPICY = "wasabi"
+        val recipe = dbHelper.getRecipe(SELECTFLAVOUR, SELECTSPICY)
+        // Wyświetlamy wynik w textView
+        textView.text = recipe
     }
 }
---
+++ app/src/main/java/com/example/pizzatopia/SQLiteHelper.kt
@@ -0,0 +1,56 @@
+package com.example.pizzatopia
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import java.io.FileOutputStream
+
+class SQLiteHelper(private val context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    companion object {
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "pizzaDB.db"
+        private const val TBL_PIZZA = "RECIPE"
+        private const val FLAVOUR = "flavour"
+        private const val SPICY = "spicy"
+        private const val RECIPE = "recipe"
+        }
+
+    override fun onCreate(db: SQLiteDatabase) {
+    }
+
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    @SuppressLint("Range")
+    fun getRecipe(selectFlavour: String, selectSpicy: String): String? {
+        copyDatabaseFile()
+        val db = readableDatabase
+        val selectRecipe = ("SELECT $RECIPE FROM $TBL_PIZZA WHERE $FLAVOUR ='$selectFlavour' AND $SPICY = '$selectSpicy'")
+        val cursor = db.rawQuery(selectRecipe, null)
+        var recipe: String? = null
+        if (cursor.moveToFirst()) {
+            recipe = cursor.getString(cursor.getColumnIndex(RECIPE))
+        }
+        cursor.close()
+        db.close()
+        return recipe
+    }
+
+    private fun copyDatabaseFile() {
+        val inputStream = context.assets.open("pizzaDB.db")
+        val outputPath = context.getDatabasePath(DATABASE_NAME).path
+        val outputStream = FileOutputStream(outputPath)
+        val buffer = ByteArray(1024)
+        var length: Int
+        while (inputStream.read(buffer).also { length = it } > 0) {
+            outputStream.write(buffer, 0, length)
+        }
+        outputStream.flush()
+        outputStream.close()
+        inputStream.close()
+    }
+}
